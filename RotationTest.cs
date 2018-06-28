03266ef7a09eacc633c5b806ff89db8b6b7b1791
Tanay Gupta <tanaygupta.gupta@gmail.com>
2018-06-28 12:58:59 +0530

    Add files via upload

---
+++ RotationTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTest : MonoBehaviour {
+
+    public Transform objecttorot;
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Moved)
+            {
+                objecttorot.transform.Rotate(0f, -touch0.deltaPosition.x, 0f);
+            }
+
+        }
+    }
+}
